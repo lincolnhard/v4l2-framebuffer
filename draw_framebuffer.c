d5c348adc32bb08409f4f892ceee3dfb5cdb642d
KwanHua Lee <lincolnhardabc@gmail.com>
2019-02-12 13:37:12 +0800

    Update draw_framebuffer.c

--- draw_framebuffer.c
+++ draw_framebuffer.c
@@ -19,37 +19,61 @@ struct fb_fix_screeninfo finfo;
 static char* fbp = NULL;
 static unsigned int screensize = 0;
 
-void free_framebuffer() {
+void free_framebuffer
+	(
+	void
+	)
+{
 	munmap(fbp, screensize);
 	close(fd);
 }
 
-void init_framebuffer() {
+void init_framebuffer
+	(
+	void
+	)
+{
 	fd = open("/dev/fb0", O_RDWR);
-	if (fd == -1) {
+	if (fd == -1) 
+		{
 		perror("Error opening framebuffer device");
 		exit(EXIT_FAILURE);
-	}
+		}
 	// Get fixed screen information
-	if (ioctl(fd, FBIOGET_FSCREENINFO, &finfo) == -1) {
+	if (ioctl(fd, FBIOGET_FSCREENINFO, &finfo) == -1) 
+		{
 		perror("Error reading fixed information");
 		exit(EXIT_FAILURE);
-	}
+		}
 	// Get variable screen information
-	if (ioctl(fd, FBIOGET_VSCREENINFO, &vinfo) == -1) {
+	if (ioctl(fd, FBIOGET_VSCREENINFO, &vinfo) == -1) 
+		{
 		perror("Error reading variable information");
 		exit(EXIT_FAILURE);
-	}
+		}
 	screensize = (vinfo.xres * vinfo.yres * vinfo.bits_per_pixel) >> 3; /* (>>3): bits to bytes */
 
 	printf("xoffset: %d, yoffset: %d\nxres: %d, yres: %d\nbits_per_pixel: %d, line_length: %d\n",
 			vinfo.xoffset, vinfo.yoffset, vinfo.xres, vinfo.yres, vinfo.bits_per_pixel, finfo.line_length);
+#if 0
+	vinfo.xres = 800;
+	vinfo.yres = 600;
+	if (ioctl(fd, FBIOPUT_VSCREENINFO, &vinfo) == -1)
+		{
+		perror("Error puting variable information");
+		exit(EXIT_FAILURE);
+		}
+	screensize = finfo.line_length * vinfo.yres;
 
-    fbp = mmap(0, screensize, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
-    if (fbp == (char *)-1) {
+	printf("xoffset: %d, yoffset: %d\nxres: %d, yres: %d\nbits_per_pixel: %d, line_length: %d\n",
+			vinfo.xoffset, vinfo.yoffset, vinfo.xres, vinfo.yres, vinfo.bits_per_pixel, finfo.line_length);
+#endif
+	fbp = mmap(0, screensize, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+	if (fbp == (char *)-1) 
+		{
 		perror("Error mapping framebuffer device to memory");
 		exit(EXIT_FAILURE);
-	}
+		}
 }
 
 void draw_framebuffer(unsigned char* src){
